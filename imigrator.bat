49c45aeebcaea6fdb0990fe6ab5a7c7829a98c22
yuusaku.hayashi <yuusaku.hayashi@outlook.jp>
2019-11-24 23:01:19 +0900

    test build

---
+++ imigrator.bat
@@ -0,0 +1,41 @@
+@echo off
+
+:: 概要
+:: 	- msysでビルドしたVimを他ユーザ環境に移行する
+:: HISTORY
+:: 	- 2019/11/24 : 新規作成
+:: 参考
+:: 	- "C:\msys64\usr\local\src\hysvim\Vim メモ.html"
+:: NOTE
+:: 	- パスは固定で入れているので、構成を変える時は修正必要
+::
+
+
+:INIT_RTN
+::openfiles > NUL 2>&1 
+::if NOT %ERRORLEVEL% EQU 0 goto MAIN_RTN
+::echo 管理者権限で実行のため、終了
+::goto EXIT_RTN
+
+:MAIN_RTN
+cd /d %~dp0
+
+set distination="C:\Program Files (x86)\Vim\vim81"
+set vim_path=C:\msys64\usr\local\src\vim
+set vim_source=%vim_path%\src
+set vim_runtime=%vim_path%\runtime
+
+mkdir		%distination% > NUL 2>&1
+cd   		%distination%
+copy /y 	%vim_source%\tee\tee.exe
+copy /y 	%vim_source%\xxd\xxd.exe
+xcopy 		%vim_runtime% %distination% /E /H /I /Y
+copy /y 	%vim_source%\*.exe
+
+:: !!! パスの指定が必要なので、実行前に入力すること !!!
+call japanese_setting.bat
+
+cd /d %~dp0
+
+:EXIT_RTN
+exit /b 0
---
+++ japanese_setting.bat
@@ -0,0 +1,39 @@
+@echo off
+
+:: 概要
+:: 	- 日本語設定を他ユーザ環境へ移行する？ 
+:: HISTORY
+:: 	- 2019/11/24 : 新規作成
+:: 参考
+:: 	- "C:\msys64\usr\local\src\hysvim\Vim_GVimのビルドと実行環境の作成 - fudist.html" 
+:: NOTE
+:: 	- 正直ほぼコピペしただけで、何をしているのか良く分からない 
+:: 	- jp.po -> jp.moにpythonを使って変換する。それをユーザ環境へコピーする
+::
+
+::openfiles > NUL 2>&1 
+::if NOT %ERRORLEVEL% EQU 0 goto MAIN_RTN
+::echo 管理者権限で実行のため、終了
+::goto END_RTN
+
+:MAIN_RTN
+cd /d %~dp0
+set distination="C:\Program Files (x86)\Vim\vim81"
+set dist_lang=%distination%\lang\ja\LC_MESSAGES
+set vim_path=C:\msys64\usr\local\src\vim
+set vim_source=%vim_path%\src
+::set vim_runtime=%vim_path%\runtime
+cd %vim_source%\po
+:: pythonがインストールされているパスを指定する
+set python_path=C:\Users\***************\AppData\Local\Programs\Python\Python36-32
+
+%python_path%\python.exe %python_path%\tools\i18n\msgfmt.py ja.po
+
+move ja.mo vim.mo
+mkdir -p %dist_lang% > NUL 2>&1
+copy /y vim.mo %dist_lang%\vim.mo 
+
+cd /d %~dp0
+
+:END_RTN
+exit /b 0
